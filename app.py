fa456ce5154c43dcf28ff90b884599daeff3e67f
Lyle Kilbey <lyle.kilbey98@gmail.com>
2025-01-31 23:20:18 +0000

    feat: added html and css for feed screen

--- app.py
+++ app.py
@@ -2,23 +2,25 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-# from flask_pymongo import PyMongo
-# from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 # from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
-# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-# app.secret_key = os.environ.get("SECRET_KEY")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
-    return "hello world!"
+    # reviews = mongo.db.reviews.find()
+    reviews = [{"name":"lyle","time":"111"}]
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
