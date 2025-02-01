92ef297a80ef6e14ef51fb6796e2ef0e0cc9e027
Lyle Kilbey <lyle.kilbey98@gmail.com>
2025-02-01 12:29:55 +0000

    feat: added registration screen and functionailty

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-# from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -31,6 +31,25 @@ def your_reviews():
 @app.route("/signin")
 def signin():
     return render_template("signin.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        print("this is a post")
+        #check if user is already registered
+        user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if user:
+            return redirect(url_for('signin'))
+    
+        new_user = {
+            "username": request.form.get("username").lower(),
+            "password":generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(new_user)
+
+        session['user'] = request.form.get("username").lower()
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
